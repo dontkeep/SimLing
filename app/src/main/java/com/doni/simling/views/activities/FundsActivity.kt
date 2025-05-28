e3b6f5ed1aa210ad37e7835b538f00bb20c63185
xsatrio <satriocahyo93@gmail.com>
2025-05-28 19:42:27 +0700

    feat: layout design xml list fund.

---
+++ app/src/main/java/com/doni/simling/views/activities/FundsActivity.kt
@@ -0,0 +1,21 @@
+package com.doni.simling.views.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.doni.simling.R
+
+class FundsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_funds)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
