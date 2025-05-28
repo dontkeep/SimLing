9add10838bf6e5294620579449ac3697acac38d9
xsatrio <satriocahyo93@gmail.com>
2025-05-28 20:32:43 +0700

    feat: functional bottom navigation

--- app/src/main/java/com/doni/simling/views/activities/MainActivity.kt
+++ app/src/main/java/com/doni/simling/views/activities/MainActivity.kt
@@ -1,21 +1,58 @@
 package com.doni.simling.views.activities
 
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.doni.simling.R
+import com.doni.simling.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.navController
+        setupSmoothBottomMenu()
+
+        val targetFragment = intent.getStringExtra("TARGET_FRAGMENT")
+        when (targetFragment) {
+            "HomeFragment" -> navController.navigate(R.id.homeFragment)
+            "ExpensesFragment" -> navController.navigate(R.id.expensesFragment)
+            "PlanFragment" -> navController.navigate(R.id.planFragment)
+            else -> navController.navigate(R.id.homeFragment)
+        }
+    }
+
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.menu)
+        val menu = popupMenu.menu
+        binding.bottomBar.setupWithNavController(menu, navController)
+        binding.bottomBar.itemIconTintActive = getColor(R.color.navIconColor)
+        binding.bottomBar.barIndicatorColor = getColor(R.color.navIndicator)
+        binding.bottomBar.setOnItemSelectedListener { position ->
+            when (position) {
+                0 -> navController.navigate(R.id.homeFragment)
+                1 -> navController.navigate(R.id.expensesFragment)
+                else -> navController.navigate(R.id.planFragment)
+            }
         }
     }
 }
