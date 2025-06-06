bfd1008943bd87e81ae33d6193bafaa379178458
xsatrio <satriocahyo93@gmail.com>
2025-06-06 14:13:37 +0700

    feat: function for fab add income button

--- app/src/main/java/com/doni/simling/views/activities/IncomeActivity.kt
+++ app/src/main/java/com/doni/simling/views/activities/IncomeActivity.kt
@@ -1,21 +1,33 @@
 package com.doni.simling.views.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.doni.simling.R
+import com.doni.simling.databinding.ActivityIncomeBinding
 
 class IncomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIncomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_income)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityIncomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            // Handle floating action button click
+            val intent = Intent(this, AddIncomeActivity::class.java)
+            startActivity(intent)
         }
     }
 }
