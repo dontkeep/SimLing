92146c75de5a217ea549569df4910e48752e6766
xsatrio <satriocahyo93@gmail.com>
2025-06-06 20:36:09 +0700

    feat: check role for add fund or income button

--- app/src/main/java/com/doni/simling/views/activities/FundsActivity.kt
+++ app/src/main/java/com/doni/simling/views/activities/FundsActivity.kt
@@ -2,14 +2,25 @@ package com.doni.simling.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.doni.simling.R
 import com.doni.simling.databinding.ActivityFundsBinding
+import com.doni.simling.helper.manager.RoleManager
+import com.doni.simling.helper.manager.RoleManager.Companion.ROLE_ADMIN
+import com.doni.simling.helper.manager.RoleManager.Companion.ROLE_WARGA
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FundsActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var roleManager: RoleManager
+
     private lateinit var binding: ActivityFundsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +32,16 @@ class FundsActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val role = roleManager.getRole()
+        when (role) {
+            ROLE_ADMIN -> {
+                binding.floatingActionButton.visibility = View.VISIBLE
+            }
+            ROLE_WARGA-> {
+                binding.floatingActionButton.visibility = View.GONE
+            }
         }
 
         binding.floatingActionButton.setOnClickListener {
--- app/src/main/java/com/doni/simling/views/activities/IncomeActivity.kt
+++ app/src/main/java/com/doni/simling/views/activities/IncomeActivity.kt
@@ -2,18 +2,28 @@ package com.doni.simling.views.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.doni.simling.R
 import com.doni.simling.databinding.ActivityIncomeBinding
+import com.doni.simling.helper.manager.RoleManager
+import com.doni.simling.helper.manager.RoleManager.Companion.ROLE_ADMIN
+import com.doni.simling.helper.manager.RoleManager.Companion.ROLE_WARGA
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class IncomeActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var roleManager: RoleManager
+
     private lateinit var binding: ActivityIncomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityIncomeBinding.inflate(layoutInflater)
@@ -24,8 +34,17 @@ class IncomeActivity : AppCompatActivity() {
             insets
         }
 
+        val role = roleManager.getRole()
+        when (role) {
+            ROLE_ADMIN -> {
+                binding.floatingActionButton.visibility = View.GONE
+            }
+            ROLE_WARGA -> {
+                binding.floatingActionButton.visibility = View.VISIBLE
+            }
+        }
+
         binding.floatingActionButton.setOnClickListener {
-            // Handle floating action button click
             val intent = Intent(this, AddIncomeActivity::class.java)
             startActivity(intent)
         }
