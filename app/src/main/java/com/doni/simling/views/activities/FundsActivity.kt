0a45327deb136831f511d91415b675b6a5bf6fe7
xsatrio <satriocahyo93@gmail.com>
2025-06-06 14:39:56 +0700

    feat: function for going to add fund fab

--- app/src/main/java/com/doni/simling/views/activities/FundsActivity.kt
+++ app/src/main/java/com/doni/simling/views/activities/FundsActivity.kt
@@ -1,21 +1,31 @@
 package com.doni.simling.views.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.doni.simling.R
+import com.doni.simling.databinding.ActivityFundsBinding
 
 class FundsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFundsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_funds)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityFundsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            val intent = Intent(this, AddFundActivity::class.java)
+            startActivity(intent)
         }
     }
 }
