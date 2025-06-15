bef0adce8f7456787edfe4024a93d87afb9001d4
xsatrio <satriocahyo93@gmail.com>
2025-06-15 14:08:32 +0700

    Added EditUserActivity and related functionality
    This commit introduces the `EditUserActivity` which allows users to edit their profile information.
    
    Key changes:
    - Added `EditUserActivity` and its layout `activity_edit_user.xml`.
    - Implemented functionality to populate fields with existing user data.
    - Added input validation for name, phone number, and password confirmation.
    - Integrated with `UserViewModel` to handle user data updates.
    - Updated `AndroidManifest.xml` to include `EditUserActivity`.
    - Added an edit icon in `DetailUserActivity` to navigate to `EditUserActivity`.
    - Renamed `UsersViewModel` to `UserViewModel` and added `editUser` function.

--- app/src/main/java/com/doni/simling/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/doni/simling/viewmodels/UserViewModel.kt
@@ -1,5 +1,6 @@
 package com.doni.simling.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,11 +9,10 @@ import androidx.paging.cachedIn
 import com.doni.simling.helper.Resource
 import com.doni.simling.models.connections.responses.DataItemUser
 import com.doni.simling.models.connections.responses.DeleteUserResponse
+import com.doni.simling.models.connections.responses.EditUserResponse
 import com.doni.simling.models.repositories.DataRepositories
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,10 +26,38 @@ class UserViewModel @Inject constructor(
     private val _deleteUser = MutableLiveData<Resource<DeleteUserResponse>>()
     val deleteUser: MutableLiveData<Resource<DeleteUserResponse>> = _deleteUser
 
+
+    private val _editUser = MutableLiveData<Resource<EditUserResponse>>()
+    val editUser: LiveData<Resource<EditUserResponse>> get() = _editUser
+
     fun deleteUser(id: Int) {
         viewModelScope.launch {
             repository.deleteUser(id).collect { resource ->
                 _deleteUser.value = resource
+            }
+        }
+    }
+
+    fun editUser(
+        id: Int,
+        name: String,
+        phoneNo: String,
+        email: String,
+        password: String,
+        address: String,
+        roleId: Int
+    ) {
+        viewModelScope.launch {
+            repository.editUser(
+                id = id,
+                name = name,
+                phoneNo = phoneNo,
+                email = email,
+                password = password,
+                address = address,
+                roleId = roleId
+            ).collect { resource ->
+                _editUser.postValue(resource)
             }
         }
     }
